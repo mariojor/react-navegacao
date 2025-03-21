8a1505ed8051947d7dd84045a5ccae538bb2fa12
malbuquerque2 <malbuquerque@opensoft.pt>
2025-03-21 14:34:53 +0000

    Use Ref

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function merge(s1, s2) {
+    return [...s1].map((e, i) => `${e}${s2[i] || ""}`).join("")
+}
+
 
 const UseRef = (props) => {
+    const [value1, setValue1] = React.useState("")
+    const [value2, setValue2] = React.useState("")
+    const count = React.useRef(0)
+
+    const myInput1 = React.useRef(null)
+    const myInput2 = React.useRef(null)
+
+    React.useEffect(function () {
+        count.current = count.current + 1
+        myInput2.current.focus()
+    }, [value1]) 
+
+    React.useEffect(function () {
+        count.current = count.current + 1
+        myInput1.current.focus()
+    }, [value2])
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exemplo 1" />
+            <div className="center">
+
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text"> {merge(value1, value2)}[</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+
+                <input ref={myInput1} type="text"
+                    className="input"
+                    value={value1}
+                    onChange={e => setValue1(e.target.value)} />
+            </div>
+            <SectionTitle title="Exemplo 2" />
+            <div className="center">
+                <input ref={myInput2} type="text" className="input" 
+                value={value2} onChange={e => setValue2(e.target.value)}/>
+            </div>
         </div>
     )
 }
