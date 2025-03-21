dbc38b84e0c8c3eb4908ef251cd684b86808dced
malbuquerque2 <malbuquerque@opensoft.pt>
2025-03-21 09:38:40 +0000

    esqueleto do projeto

---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (props) => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Bem-vindo!</p>
+        </div>
+    )
+}
+
+export default Home;
