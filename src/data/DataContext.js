b4327b8dc07b0d926cdf7a2c084174d0a44f488d
malbuquerque2 <malbuquerque@opensoft.pt>
2025-03-24 15:20:32 +0000

    usememo usecalback usecontext

---
+++ src/data/DataContext.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const data = {
+    title: 'Hook UseCallback',
+    number: 0,
+
+}
+
+const DataContext = React.createContext(data)
+
+export default DataContext
+
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import "./index.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
+import DataContext, { data } from './data/DataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <App />
+
 );
 
